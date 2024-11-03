6009cbaf95b643a7d4490494c946ab59f1b5bac5
pedrosfaria2 <pedroserrano2@gmail.com>
2024-11-03 19:08:12 -0300

    refactor: refactor bot handlers initialization

--- src/infra/bot/bot_builder.js
+++ src/infra/bot/bot_builder.js
@@ -2,8 +2,9 @@ const StartHandler = require('./handlers/start_handler');
 const HelpHandler = require('./handlers/help/help_handler');
 const ReminderHandler = require('./handlers/reminder/reminder_handler');
 const WhatsForTheDayHandler = require('./handlers/reminder/whats_for_the_day_handler');
-const OpenRemindersHandler = require('./handlers/reminder/open_reminds_handler');
 const ShoppingListHandler = require('./handlers/shopping_list/shopping_list_handler');
+const OpenRemindersHandler = require('./handlers/reminder/open_reminds_handler');
+const MessageInterpreter = require('../router/message_interpreter');
 
 class BotBuilder {
     constructor(bot, reminderService, shoppingListService, logger) {
@@ -11,41 +12,62 @@ class BotBuilder {
         this.reminderService = reminderService;
         this.shoppingListService = shoppingListService;
         this.logger = logger;
+
+        this.messageInterpreter = new MessageInterpreter(
+            bot,
+            shoppingListService,
+            reminderService,
+            logger
+        );
     }
 
     build() {
         this.logger.info('Starting to build bot handlers');
 
-        const handlers = [
-            new StartHandler(this.bot, this.logger),
-            new HelpHandler(this.bot, this.logger),
-            new ReminderHandler(this.bot, this.reminderService, this.logger),
-            new WhatsForTheDayHandler(
-                this.bot,
-                this.reminderService,
-                this.logger
-            ),
-            new OpenRemindersHandler(
-                this.bot,
-                this.reminderService,
-                this.logger
-            ),
-            new ShoppingListHandler(
-                this.bot,
-                this.shoppingListService,
-                this.logger
-            ),
+        const handlerConfigs = [
+            {
+                handlerClass: StartHandler,
+                params: [this.bot, this.logger],
+            },
+            {
+                handlerClass: HelpHandler,
+                params: [this.bot, this.logger],
+            },
+            {
+                handlerClass: ReminderHandler,
+                params: [this.bot, this.reminderService, this.logger],
+            },
+            {
+                handlerClass: WhatsForTheDayHandler,
+                params: [this.bot, this.reminderService, this.logger],
+            },
+            {
+                handlerClass: ShoppingListHandler,
+                params: [this.bot, this.shoppingListService, this.logger],
+            },
+            {
+                handlerClass: OpenRemindersHandler,
+                params: [this.bot, this.reminderService, this.logger],
+            },
         ];
 
-        handlers.forEach(handler => {
+        handlerConfigs.forEach(config => {
+            const handler = new config.handlerClass(...config.params);
+            handler.register();
+        });
+
+        this.bot.on('message', async ctx => {
             try {
-                handler.register();
-                this.logger.info(
-                    `${handler.constructor.name} registered successfully`
-                );
+                if (ctx.message.text.startsWith('/')) {
+                    return;
+                }
+                await this.messageInterpreter.interpret(ctx);
             } catch (error) {
                 this.logger.error(
-                    `Error registering ${handler.constructor.name}: ${error.message}`
+                    `Error handling user message: ${error.message}`
+                );
+                await ctx.reply(
+                    'An error occurred while processing your request. Please try again later.'
                 );
             }
         });
